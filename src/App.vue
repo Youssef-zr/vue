c28b9b087aa1d4c73b13f2cc26f09df92d20c2cb
youssefzr <yn-neinaa@hotmail.com>
2021-06-15 13:59:31 +0100

    add spinner

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <div class="spinner">
+      <fingerprint-spinner
+        :animation-duration="1500"
+        :size="64"
+        color="#009dea"
+      />
+    </div>
     <!-- navigation bar -->
     <navigation></navigation>
 
@@ -15,12 +22,13 @@
 <script>
 import navigation from './components/layouts/nav'
 import footeer from './components/layouts/footer'
-
+import { FingerprintSpinner } from 'epic-spinners'
 export default {
   name: 'app',
   components: {
     navigation,
-    footeer
+    footeer,
+    FingerprintSpinner,
   },
 }
 </script>
@@ -31,5 +39,21 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+}
+
+body {
+  overflow: hidden;
+}
+.spinner {
+  position: fixed;
+  top: 0;
+  left: 0;
+  background: #fff;
+  width: 100%;
+  height: 100vh;
+  z-index: 99999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
